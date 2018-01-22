f41e0ce1677a1322e1e600be5f6833af5bb40a41
qaraluch <prezenciq@gmail.com>
2018-01-22 09:25:19 +0100

    Import code from ytdlv3 project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-function XXX() {
-  return {};
-}
+const hyphenTypes = [
+  "\u002D",
+  "\u2010",
+  "\u2011",
+  "\u2012",
+  "\u2013",
+  "\u2014",
+  "\u2015",
+  "\u2E3A",
+  "\u2E3B",
+  "\uFE58",
+  "\uFE63",
+  "\uFF0D",
+];
 
-export default XXX;
+export default function splitByHyphen(str, titleSplitAt = 0) {
+  const ifCharIsH = (ch) => hyphenTypes
+    .map(h => ch === h)
+    .reduce((acc, cur) => cur === true ? true : acc);
+  let positionsH = str.split("")
+    .map(char => ifCharIsH(char))
+    .map((bool, idx) => bool && idx)
+    .filter(bool => bool !== false);
+
+  const filterForTitleSplit = (positionsH, titleSplitAt) => {
+    const lgh = positionsH.length;
+    return titleSplitAt === 0 ? positionsH :
+      (titleSplitAt > 0 && titleSplitAt < lgh) ? positionsH.filter((posNo, idx) => idx === titleSplitAt - 1) :
+        positionsH;
+  };
+
+  positionsH = filterForTitleSplit(positionsH, titleSplitAt);
+
+  const chunks = str
+    .split("")
+    .map((char, idx) => positionsH.includes(idx) ? "|" : char)
+    .join("")
+    .split("|");
+  // remove empty strings
+  const cleanChunks = chunks.filter(chunk => chunk).map(chunk => chunk.trim());
+
+  return positionsH.length === 0 ? undefined : cleanChunks;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,129 @@
 import test from "ava";
-import XXX from "../dist/index.js";
+import splitByHyphen from "../dist/qm-txt-splitbyhyphen.js";
 
-test("constructor function check", t => {
-  const msg = "should be a function i.e. construction fn.";
-  const actual = typeof XXX === "function";
+test("is function", t => {
+  const msg = "should be a function ";
+  const actual = typeof splitByHyphen === "function";
   const expected = true;
   t.is(actual, expected, msg);
 });
 
-test("init", t => {
-  const msg1 = "should construction fn. return an object i.e. state";
-  const actual1 = typeof XXX() === "object";
-  const expected1 = true;
-  t.is(actual1, expected1, msg1);
+test("split by hyphen-minus (default and the most popular)", t => {
+  t.deepEqual(splitByHyphen("tere-fere"), ["tere", "fere"]);
+  t.deepEqual(splitByHyphen("tere - fere"), ["tere", "fere"]);
+  t.deepEqual(splitByHyphen("     tere -      fere      "), ["tere", "fere"]);
 });
+
+test("find no hyphens", t => {
+  const msg = "should return undefined when find no hyphens in the string";
+  t.is(splitByHyphen("tere \u1F384 fere"), undefined, msg);
+  t.is(splitByHyphen("tere\u1F384fere"), undefined, msg);
+});
+
+test("any kind of hyphen", t => {
+  t.deepEqual(splitByHyphen("tere - fere"), ["tere", "fere"], "should split by U+002D	HYPHEN-MINUS");
+  t.deepEqual(splitByHyphen("tere ‐ fere"), ["tere", "fere"], "should split by U+2010	HYPHEN");
+  t.deepEqual(splitByHyphen("tere ‑ fere"), ["tere", "fere"], "should split by U+2011	NON-BREAKING HYPHEN");
+  t.deepEqual(splitByHyphen("tere ‒ fere"), ["tere", "fere"], "should split by U+2012	FIGURE DASH");
+  t.deepEqual(splitByHyphen("tere – fere"), ["tere", "fere"], "should split by U+2013	EN DASH");
+  t.deepEqual(splitByHyphen("tere — fere"), ["tere", "fere"], "should split by U+2014	EM DASH");
+  t.deepEqual(splitByHyphen("tere ― fere"), ["tere", "fere"], "should split by U+2015	HORIZONTAL BAR");
+  t.deepEqual(splitByHyphen("tere ⸺ fere"), ["tere", "fere"], "should split by U+2E3A	TWO-EM DASH");
+  t.deepEqual(splitByHyphen("tere ⸻ fere"), ["tere", "fere"], "should split by U+2E3B	THREE-EM DASH");
+  t.deepEqual(splitByHyphen("tere ﹘ fere"), ["tere", "fere"], "should split by U+FE58	SMALL EM DASH");
+  t.deepEqual(splitByHyphen("tere ﹣ fere"), ["tere", "fere"], "should split by U+FE63	SMALL HYPHEN-MINUS");
+  t.deepEqual(splitByHyphen("tere － fere"), ["tere", "fere"], "should split by U+FF0D	FULLWIDTH HYPHEN-MINUS");
+});
+
+test("hyphenfied string", t => {
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g-"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+  t.deepEqual(splitByHyphen("a-b-c---d-e-f-g-"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+  t.deepEqual(splitByHyphen("a-b‐c‑d‒e–f⸺g"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+});
+
+test("hyphen at the beginig", t => {
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+  t.deepEqual(splitByHyphen("-a-b-c-d-e-f-g"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+  t.deepEqual(splitByHyphen("--a-b-c-d-e-f-g"), ["a", "b", "c", "d", "e", "f", "g"], "should return array of words");
+});
+
+
+test("title split", t => {
+  const titleSplitAt_Off = 0;
+  const titleSplitAt_Default = 1;
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g", titleSplitAt_Off), ["a", "b", "c", "d", "e", "f", "g"], "should not title split");
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g", titleSplitAt_Default), ["a", "b-c-d-e-f-g"], "should split in two at first hyphen (default)");
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g", 2), ["a-b", "c-d-e-f-g"], "should split in two at secound hyphen");
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g", 3), ["a-b-c", "d-e-f-g"], "should split in two at third hyphen ");
+  t.deepEqual(splitByHyphen("a-b-c-d-e-f-g", 8), ["a", "b", "c", "d", "e", "f", "g"], "should not title split ");
+});
+
+
+
+// ["a","b","c","d","e","f","g"]
+//a
+//b
+//c
+//d
+//e
+//f
+//g
+
+
+
+//"a-b‐c‑d‒e–f⸺g"
+
+//-
+//‐
+//‑
+//‒
+//–
+//—
+//―
+//⸺
+//⸻
+//﹘
+//﹣
+//－
+
+
+
+// "\u002D"
+// "\u2010"
+// "\u2011"
+// "\u2012"
+// "\u2013"
+// "\u2014"
+// "\u2015"
+// "\u2E3A"
+// "\u2E3B"
+// "\uFE58"
+// "\uFE63"
+// "\uFF0D"
+
+// U+002D	HYPHEN-MINUS
+// U+2010	HYPHEN
+// U+2011	NON-BREAKING HYPHEN
+// U+2012	FIGURE DASH
+// U+2013	EN DASH
+// U+2014	EM DASH
+// U+2015	HORIZONTAL BAR
+// U+2E3A	TWO-EM DASH
+// U+2E3B	THREE-EM DASH
+// U+FE58	SMALL EM DASH
+// U+FE63	SMALL HYPHEN-MINUS
+// U+FF0D	FULLWIDTH HYPHEN-MINUS
+
+// U+002D	HYPHEN-MINUS	(-)	
+// U+2010	HYPHEN	(‐)	
+// U+2011	NON-BREAKING HYPHEN	(‑)	
+// U+2012	FIGURE DASH	(‒)	
+// U+2013	EN DASH	(–)	
+// U+2014	EM DASH	(—)	
+// U+2015	HORIZONTAL BAR	(―)	
+// U+2E3A	TWO-EM DASH	(⸺)	
+// U+2E3B	THREE-EM DASH	(⸻)	
+// U+FE58	SMALL EM DASH	(﹘)	
+// U+FE63	SMALL HYPHEN-MINUS	(﹣)	
+// U+FF0D	FULLWIDTH HYPHEN-MINUS	(－)	
